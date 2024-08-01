5422f5dbec1cd508a7b8be22245b7463c48f48cb
JIAMENEZES123 <23b26.jia@sjec.ac.in>
2024-08-01 17:57:13 +0530

    Changes made

--- src/app/product/[id]/page.jsx
+++ src/app/product/[id]/page.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const page = () => {
   return (
     <>
-      <h1>Product page </h1>
-      <p>this is an individual product page </p>
+      <h1 className="text-red font-medium text-lg">Product page </h1>
+      <p className="text-black text-sm">this is an individual product page </p>
     </>
   );
 };
